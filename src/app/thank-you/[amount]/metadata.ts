abc1ef730e34d5834a939055263b5ed060fdc53c
JR Bussard <scottbussardjr@gmail.com>
2025-06-25 06:04:39 -0400

    Testing sharing mk2

---
+++ src/app/thank-you/[amount]/metadata.ts
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+export async function generateMetadata({ params }: { params: { amount: string } }): Promise<Metadata> {
+    const amount = params.amount || "1";
+    return {
+        title: `I've given $${amount} on gimme.jrbussard.com, you should too!`,
+        openGraph: {
+            images: [
+                {
+                    url: `https://gimme.jrbussard.com/api/og?amount=${amount}`,
+                    width: 1200,
+                    height: 630,
+                    alt: `I've given $${amount} on gimme.jrbussard.com, you should too!`,
+                },
+            ],
+        },
+        other: {
+            "og:image:width": "1200",
+            "og:image:height": "630",
+        },
+    };
+}
